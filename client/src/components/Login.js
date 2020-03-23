9d86e1bde7f90765ec26f8aacd0b2d30b84845d3
MLucky518 <lucky32815@gmail.com>
2020-03-22 20:39:58 -0400

    mvp/stretch

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -5,9 +5,9 @@ import { useHistory } from "react-router-dom";
 const Login = () => {
   const history = useHistory();
 
-  const [user,setUser] = useState({
-    username:"",
-    password:""
+  const [user, setUser] = useState({
+    username: "",
+    password: ""
   });
 
   const handleChanges = e => {
@@ -18,38 +18,46 @@ const Login = () => {
     e.preventDefault();
 
     axios
-    .post("http://localhost:5000/api/login",user)
-    .then(res=>{
+      .post("http://localhost:5000/api/login", user)
+      .then(res => {
         console.log(res);
-        window.localStorage.setItem("token",res.data.payload);
+        window.localStorage.setItem("token", res.data.payload);
         history.push("/protected");
-    })
-    .catch(err =>{
+      })
+      .catch(err => {
         console.log(err);
-    })
+      });
   };
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
-    <div>
-      <form onSubmit={login}>
-        <input
-          type="text"
-          name="username"
-          placeholder="username"
-          onChange={handleChanges}
-          value={user.username}
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="password"
-          onChange={handleChanges}
-          value={user.password}
-        />
-        <input type="submit" />
+    <div className = "form-container">
+      <h1>Login</h1>
+   
+      
+      <form className="ui form" onSubmit={login}>
+        <div className="field">
+          <input
+            type="text"
+            name="username"
+            placeholder="username"
+            onChange={handleChanges}
+            value={user.username}
+          />
+        </div>
+        <div className="field">
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            onChange={handleChanges}
+            value={user.password}
+          />
+        </div>
+        <button className="ui button" type="submit" >SUBMIT</button>
       </form>
     </div>
+    
   );
 };
 
